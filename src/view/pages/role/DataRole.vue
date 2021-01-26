ac1d7ac18e55cb07679288aeb13f3564b940366e
triandamai <triannurizkillah@gmail.com>
2021-01-26 12:54:36 -0500

    [UPDATE] post,put,del role  done resolve #3 commit 2781e99e2cc2590b5ba0becfd46486fa9ac3ffbf

--- src/view/pages/role/DataRole.vue
+++ src/view/pages/role/DataRole.vue
@@ -13,7 +13,15 @@
         </span>
       </div>
       <div class="alert-text">
-        <b-button @click="dialog = !dialog" class="mr-3" variant="success"
+        <b-button
+          @click="
+            {
+              dialog = !dialog;
+              isEdit = false;
+            }
+          "
+          class="mr-3"
+          variant="success"
           >Tambahkan Role Baru</b-button
         >
 
@@ -62,7 +70,19 @@
                 aria-label="Toolbar with button groups and dropdown menu"
               >
                 <b-button-group class="mx-1">
-                  <b-button @click="dialog = !dialog">Ubah</b-button>
+                  <b-button
+                    @click="
+                      {
+                        dialog = !dialog;
+                        isEdit = true;
+                        name = item.name;
+                        id = item.id;
+                        description = item.description;
+                      }
+                    "
+                    type="button"
+                    >Ubah</b-button
+                  >
                   <b-button>Detail</b-button>
                   <b-button @click="deleteRole(item)">Hapus</b-button>
                 </b-button-group>
@@ -76,39 +96,52 @@
     <v-row justify="center">
       <v-dialog v-model="dialog" persistent max-width="600px">
         <v-card>
-          <v-card-title>
-            <span class="headline">User Profile</span>
-          </v-card-title>
-          <v-card-text>
-            <v-container>
-              <v-row>
-                <v-col cols="12">
-                  <v-text-field
-                    type="text"
-                    label="Nama*"
-                    required
-                  ></v-text-field>
-                </v-col>
-                <v-col cols="12">
-                  <v-text-field
-                    label="Deskripsi*"
-                    type="text"
-                    required
-                  ></v-text-field>
-                </v-col>
-              </v-row>
-            </v-container>
-            <small>*indicates required field</small>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
-            <v-btn color="blue darken-1" text @click="dialog = false"
-              >Close</v-btn
-            >
-            <v-btn color="blue darken-1" text @click="dialog = false"
-              >Save</v-btn
-            >
-          </v-card-actions>
+          <form @submit.prevent="saveRole">
+            <v-card-title>
+              <span class="headline">User Profile</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col cols="12">
+                    <v-text-field
+                      type="text"
+                      label="Nama*"
+                      v-model="name"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col cols="12">
+                    <v-text-field
+                      label="Deskripsi*"
+                      type="text"
+                      v-model="description"
+                      required
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+              <small>*indicates required field</small>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="blue darken-1"
+                tye="button"
+                text
+                @click="
+                  {
+                    dialog = false;
+                    isEdit = false;
+                    name = null;
+                    description = null;
+                  }
+                "
+                >Close</v-btn
+              >
+              <v-btn color="blue darken-1" text type="submit">Save</v-btn>
+            </v-card-actions>
+          </form>
         </v-card>
       </v-dialog>
     </v-row>
@@ -116,11 +149,25 @@
 </template>
 <script>
 /*eslint-disable*/
-import { ACTION_GET_ROLE } from "@/store";
+import {
+  ACTION_GET_DATA_SYSTEM,
+  SROLE,
+  ACTION_PUT_DATA_SYSTEM,
+  ACTION_POST_DATA_SYSTEM,
+  ACTION_DELETE_DATA_SYSTEM,
+} from "@/store";
 import { mapState } from "vuex";
 import Swal from "sweetalert2";
 export default {
   name: "RoleSystem",
+  data: () => {
+    return {
+      isEdit: false,
+      id: "",
+      name: "",
+      description: "",
+    };
+  },
   computed: {
     ...mapState({
       items: (state) => state.system.dataroles,
@@ -144,15 +191,107 @@ export default {
     },
   },
   created() {
-    this.getUsers();
+    this.getRoles();
   },
   methods: {
-    getUsers() {
-      this.$store.dispatch("system/" + ACTION_GET_ROLE).then((res) => {
-        if (res) {
-          this.getUsers();
-        }
-      });
+    getRoles() {
+      this.$store
+        .dispatch("system/" + ACTION_GET_DATA_SYSTEM, {
+          systemtype: SROLE,
+          path: "roles",
+          id: null,
+        })
+        .then((res) => {
+          if (res) {
+            this.getUsers();
+          }
+        });
+    },
+    saveRole() {
+      if (this.isEdit) {
+        this.$store
+          .dispatch("system/" + ACTION_PUT_DATA_SYSTEM, {
+            systemtype: SROLE,
+            path: "role",
+            body: {
+              id: this.id,
+              name: this.name,
+              description: this.description,
+            },
+          })
+          .then(({ success, message }) => {
+            if (success) {
+              Swal.fire({
+                title: "",
+                text: message,
+                icon: "success",
+                confirmButtonText: "Oke",
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  this.dialog = "";
+                  this.description = "";
+                  this.isEdit = false;
+                  this.id = "";
+                }
+              });
+            } else {
+              Swal.fire({
+                title: "",
+                text: message,
+                icon: "error",
+                confirmButtonText: "Oke",
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  this.dialog = "";
+                  this.description = "";
+                  this.isEdit = false;
+                  this.id = "";
+                }
+              });
+            }
+          });
+      } else {
+        this.$store
+          .dispatch("system/" + ACTION_POST_DATA_SYSTEM, {
+            systemtype: SROLE,
+            path: "role",
+            body: {
+              name: this.name,
+              description: this.description,
+            },
+          })
+          .then(({ success, message }) => {
+            if (success) {
+              Swal.fire({
+                title: "",
+                text: message,
+                icon: "success",
+                confirmButtonText: "Oke",
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  this.dialog = "";
+                  this.description = "";
+                  this.isEdit = false;
+                  this.id = "";
+                }
+              });
+            } else {
+              Swal.fire({
+                title: "",
+                text: message,
+                icon: "error",
+                confirmButtonText: "Oke",
+              }).then((result) => {
+                if (result.isConfirmed) {
+                  this.dialog = "";
+                  this.description = "";
+                  this.isEdit = false;
+                  this.id = "";
+                }
+              });
+            }
+          });
+      }
     },
     deleteRole(role) {
       Swal.fire({
@@ -165,6 +304,35 @@ export default {
         confirmButtonText: "Oke, hapus!",
       }).then((result) => {
         if (result.isConfirmed) {
+          this.$store
+            .dispatch("system/" + ACTION_DELETE_DATA_SYSTEM, {
+              systemtype: SROLE,
+              path: "role",
+              body: { id: role.id },
+            })
+            .then(({ success, message }) => {
+              if (success) {
+                Swal.fire({
+                  title: "",
+                  text: message,
+                  icon: "success",
+                  confirmButtonText: "Oke",
+                }).then((result) => {
+                  if (result.isConfirmed) {
+                  }
+                });
+              } else {
+                Swal.fire({
+                  title: "",
+                  text: message,
+                  icon: "error",
+                  confirmButtonText: "Oke",
+                }).then((result) => {
+                  if (result.isConfirmed) {
+                  }
+                });
+              }
+            });
         }
       });
     },
