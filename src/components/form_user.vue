2903a67e493bc6738f703c195f3229f27c95be57
triandamai <triannurizkillah@gmail.com>
2021-04-22 21:59:19 +0700

    feat(user): add functionality user

---
+++ src/components/form_user.vue
@@ -0,0 +1,109 @@
+<template>
+  <v-dialog
+    transition="dialog-bottom-transition"
+    max-width="600"
+    v-model="show"
+    persistent
+  >
+    <template v-slot:default="dialog">
+      <v-card>
+        <v-toolbar color="primary" dark>Form User</v-toolbar>
+        <v-card-text>
+          <v-container>
+            <v-row>
+              <v-col cols="12">
+                <v-text-field
+                  v-model="form.username"
+                  label="Username*"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12">
+                <v-text-field
+                  v-model="form.email"
+                  type="email"
+                  label="Email*"
+                  required
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" v-show="!isEdit">
+                <v-text-field
+                  v-model="form.password"
+                  type="password"
+                  label="Password*"
+                  required
+                ></v-text-field>
+              </v-col>
+
+              <v-col cols="12">
+                <v-autocomplete
+                  v-model="form.role_id"
+                  :items="itemsrole"
+                  item-value="id"
+                  item-text="name"
+                  label="Role*"
+                  required
+                ></v-autocomplete>
+              </v-col>
+              <v-col cols="12">
+                <v-autocomplete
+                  v-model="form.group_id"
+                  :items="itemsgroup"
+                  item-value="id"
+                  item-text="name"
+                  label="Group*"
+                  required
+                ></v-autocomplete>
+              </v-col>
+            </v-row>
+          </v-container>
+          <small>*indicates required field</small>
+        </v-card-text>
+        <v-card-actions class="justify-end">
+          <v-spacer></v-spacer>
+          <v-btn
+            color="blue darken-1"
+            text
+            @click="$emit('close', dialog.value)"
+          >
+            Close
+          </v-btn>
+          <v-btn color="blue darken-1" text @click="submit"> Save </v-btn>
+        </v-card-actions>
+      </v-card>
+    </template>
+  </v-dialog>
+</template>
+<script>
+import { mapState } from "vuex";
+export default {
+  props: ["show", "body", "isEdit"],
+  data: () => {
+    return {
+      form: {
+        username: "",
+        email: "",
+        role_id: "",
+        group_id: "",
+        active: 1,
+      },
+    };
+  },
+  computed: {
+    ...mapState({
+      itemsrole: (state) => state.system.dataroles,
+      itemsgroup: (state) => state.system.datagroups,
+    }),
+  },
+  watch: {
+    body: function (newVal, oldVal) {
+      this.form = newVal;
+    },
+  },
+  methods: {
+    submit() {
+      this.$emit("submit", this.form);
+    },
+  },
+};
+</script>
