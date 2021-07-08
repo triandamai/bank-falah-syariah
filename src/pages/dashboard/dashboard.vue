4e87b107d0048efe73327776a18689ba74459db0
triandamai <triannurizkillah@gmail.com>
2021-07-08 23:22:58 +0700

    feat: add chart

--- src/pages/dashboard/dashboard.vue
+++ src/pages/dashboard/dashboard.vue
@@ -15,17 +15,14 @@
                     <p class="font-roboto">Overview of last month</p>
                   </div>
                   <div class="col-xl-12 p-0 left_side_earning">
-                    <h5>$4055.56</h5>
-                    <p class="font-roboto">This Month Earning</p>
+                    <h5>4055.56</h5>
+                    <p class="font-roboto">Nasabah</p>
                   </div>
                   <div class="col-xl-12 p-0 left_side_earning">
-                    <h5>$1004.11</h5>
-                    <p class="font-roboto">This Month Profit</p>
+                    <h5>1004.11</h5>
+                    <p class="font-roboto">Transaksi</p>
                   </div>
-                  <div class="col-xl-12 p-0 left_side_earning">
-                    <h5>90%</h5>
-                    <p class="font-roboto">This Month Sale</p>
-                  </div>
+
                   <div class="col-xl-12 p-0 left-btn">
                     <a class="btn btn-gradient">Summary</a>
                   </div>
@@ -57,52 +54,17 @@
                   </div>
                   <div class="row">
                     <div class="col-xl-12">
-                      <div class="card-body p-0">
+                      <div class="card-body pt-5 pb-5">
                         <div class="current-sale-container">
                           <div id="chart-currently">
                             <apexchart
-                              height="255"
+                              height="250"
                               type="area"
                               :options="apexDashboard.options"
                               :series="apexDashboard.series"
                             ></apexchart>
                           </div>
                         </div>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div class="row border-top m-0">
-                  <div class="col-xl-4 pl-0 col-md-6 col-sm-6">
-                    <div class="media p-0">
-                      <div class="media-left">
-                        <i class="icofont icofont-crown"></i>
-                      </div>
-                      <div class="media-body">
-                        <h6>Referral Earning</h6>
-                        <p>$5,000.20</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-xl-4 col-md-6 col-sm-6">
-                    <div class="media p-0">
-                      <div class="media-left bg-secondary">
-                        <i class="icofont icofont-heart-alt"></i>
-                      </div>
-                      <div class="media-body">
-                        <h6>Cash Balance</h6>
-                        <p>$2,657.21</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="col-xl-4 col-md-12 pr-0">
-                    <div class="media p-0">
-                      <div class="media-left">
-                        <i class="icofont icofont-cur-dollar"></i>
-                      </div>
-                      <div class="media-body">
-                        <h6>Sales forcasting</h6>
-                        <p>$9,478.50</p>
                       </div>
                     </div>
                   </div>
@@ -226,11 +188,11 @@ export default {
         },
         series: [
           {
-            name: "series1",
+            name: "Nasabah",
             data: [6, 20, 15, 40, 18, 20, 18, 23, 18, 35, 30, 55, 0],
           },
           {
-            name: "series2",
+            name: "Transaksi",
             data: [2, 22, 35, 32, 40, 25, 50, 38, 42, 28, 20, 45, 0],
           },
         ],
