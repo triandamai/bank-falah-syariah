1b404a5edc2834b93769697d3e526d25cf4291ef
trian damai <triannurizkillah@gmail.com>
2022-01-02 18:05:48 +0700

    feat: add angsuran history(#72)
    Refs: #72
    working on progress

--- src/pages/detail-rekening/detail-rekening-pembiayaan.vue
+++ src/pages/detail-rekening/detail-rekening-pembiayaan.vue
@@ -27,6 +27,114 @@
             </div>
           </div>
         </div>
+        <!--        detail-->
+        <div class="col-xl-9 col-md-8 col-sm-12 col-xs-12 box-col-12">
+          <div class="file-content">
+            <div class="card">
+              <div class="card-body">
+                <div class="invoice">
+                  <div>
+                    <div>
+                      <div class="row">
+                        <div class="col-sm-6">
+                          <div class="media">
+                            <div class="media-left">
+                              <img
+                                  class="media-object img-60"
+                                  src="../../assets/images/logo/logo.webp"
+                                  alt
+                              />
+                            </div>
+                            <div class="media-body m-l-20">
+                              <h5 class="media-heading">Bank Falah Syariah</h5>
+                              <p>
+                                Bakaran Project
+                                <br />
+                                <span class="digits">(+62)81-226-xxx-xxx</span>
+                              </p>
+                            </div>
+                          </div>
+                          <!-- End Info-->
+                        </div>
+                        <div class="col-sm-6">
+                          <div class="text-md-right">
+                            <h3>
+                              {{nasabah.nama_lengkap}}
+                              <span class="digits counter">#{{ decrypt($route.query.r) }}</span>
+                            </h3>
+                            <p>
+                              {{getMonthString()}}
+                              <span class="digits">{{ getTodayDateMutasi() }}</span>
+                              <br />
+                            </p>
+                          </div>
+                          <!-- End Title-->
+                        </div>
+                      </div>
+                    </div>
+                    <hr />
+                    <div>
+                      <div class="table-responsive invoice-table" id="table">
+                        <table class="table table-bordered table-striped">
+                          <tbody>
+                          <tr>
+                            <td class="item">
+                              <h6 class="p-2 mb-0">Tanggal Transaksi</h6>
+                            </td>
+                            <td class="Hours">
+                              <h6 class="p-2 mb-0">Tipe</h6>
+                            </td>
+                            <td class="Rate">
+                              <h6 class="p-2 mb-0">Jenis Transaksi</h6>
+                            </td>
+                            <td class="subtotal">
+                              <h6 class="p-2 mb-0">Angsuran</h6>
+                            </td>
+                            <td class="subtotal">
+                              <h6 class="p-2 mb-0">Angsuran Harus Dibayar</h6>
+                            </td>
+                          </tr>
+                          <tr v-for="(itemsMutasiPembiayaan,index) in mutasi" :key="index">
+                            <td>
+                              <label>{{itemsMutasiPembiayaan.tgl_transaksi}}</label>
+                            </td>
+                            <td>
+                              <p class="itemtext digits">{{ getTypeTransaction(itemsMutasiPembiayaan.type) }}</p>
+                            </td>
+                            <td>
+                              <p class="itemtext digits">{{itemsMutasiPembiayaan.jenis_transaksi.nama_transaksi}}</p>
+                            </td>
+                            <td>
+                              <p class="itemtext digits">Rp {{formatCurrency(itemsMutasiPembiayaan.value) }}</p>
+                            </td>
+                            <td>
+                              <p class="itemtext digits">Rp {{formatCurrency(itemsMutasiPembiayaan.saldo) }}</p>
+                            </td>
+                          </tr>
+
+                          </tbody>
+                        </table>
+                      </div>
+                      <!-- End Table-->
+
+                    </div>
+                    <!-- End InvoiceBot-->
+                  </div>
+                  <div class="col-sm-12 text-center mt-3">
+
+                    <button class="btn btn-secondary" @click="$router.go(-1)" type="button">kembali</button>
+                  </div>
+                  <!-- End Invoice-->
+                  <!-- End Invoice Holder-->
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+        </div>
+        <!--        -->
+<!--        detail-->
         <div class="col-xl-9 col-md-8 col-sm-12 col-xs-12 box-col-12">
           <div class="file-content">
             <div class="card">
@@ -132,6 +240,7 @@
 
           </div>
         </div>
+<!--        -->
       </div>
     </div>
     <!-- Container-fluid Ends-->
